c2ddecfe0ac85b1e4225d4249f00be4c04596ad7
Line Svensen <144799112+LineSvensen@users.noreply.github.com>
2024-11-30 09:15:26 +0100

    vite config added. for some reason was not there.

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import path from 'path';
+
+export default defineConfig({
+  root: '.',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        createListing: path.resolve(__dirname, 'pages/create-listing.html'),
+        listingDetails: path.resolve(__dirname, 'pages/listing-details.html'),
+        loginRegister: path.resolve(__dirname, 'pages/login-register.html'),
+        profile: path.resolve(__dirname, 'pages/profile.html'),
+      },
+    },
+  },
+});
